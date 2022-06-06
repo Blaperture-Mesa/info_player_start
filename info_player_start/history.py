7898088fd57f508b6f10439cd683a09e09dc3535
AnggaraNothing <anggarayamap@protonmail.com>
2022-06-06 07:05:05 +0700

    Added csonews stuffs

---
+++ info_player_start/history.py
@@ -0,0 +1,103 @@
+import os
+import pathlib
+import asyncio
+from mautrix.client import Client
+from mautrix.types import (
+    SyncToken
+    , PaginationDirection
+    , BaseRoomEvent
+    , MessageEvent
+    , RedactionEvent
+    , TextMessageEventContent
+    , RelatesTo
+    , RelationType
+)
+from . import config, app
+
+
+
+BM_IPS_ROOM_STATS_ID = os.environ.get( "BM_IPS_ROOM_STATS_ID" )
+HISTORY_PATH = pathlib.Path( "bm.ips.history" )
+if config.USE_JSON:
+    HISTORY_PATH = pathlib.Path( "/tmp/bm/ips/history.json" )
+
+
+def write_history (event: MessageEvent):
+    config.datastore_write( HISTORY_PATH, {
+        "event_id": event.event_id
+    } )
+
+
+def read_history ():
+    msg = MessageEvent(
+        None , None , None , None , None , None
+    )
+    try:
+        msg.event_id = config.datastore_load( HISTORY_PATH )["event_id"]
+    except:
+        app.LOGGER.exception( "Failed to load history" )
+    return msg
+
+
+async def aiter_messages (client: Client, *args, **kwargs):
+    await client.sync( full_state=True )
+    token: SyncToken = None
+    while (
+        msg := await client.get_messages(
+            *args
+            , **kwargs
+            , from_token=token
+        )
+    ).end != token:
+        token = msg.end
+        events: list[BaseRoomEvent] = msg.events
+        for evt in events:
+            yield evt
+
+
+CLIENT_HISTORY_MSG_EVT = read_history()
+
+
+async def get_history_id (room_id):
+    global CLIENT_HISTORY_MSG_EVT
+    oeid = old_evt_id = CLIENT_HISTORY_MSG_EVT.event_id or None
+    if old_evt_id:
+        async for evt in aiter_messages( app.CLIENT, room_id, PaginationDirection.BACKWARD ):
+            if isinstance( evt, RedactionEvent ):
+                if evt.redacts != old_evt_id:
+                    continue
+                old_evt_id = None
+                break # deleted.
+            elif isinstance( evt, MessageEvent ):
+                msg_content = evt.content
+                if not isinstance( msg_content, TextMessageEventContent ):
+                    continue
+                if evt.event_id != old_evt_id:
+                    continue
+                break # found.
+        if not old_evt_id:
+            app.LOGGER.warning( "update:Message %s is not found/deleted", oeid )
+    return old_evt_id
+
+
+async def edit_or_write_history (room_id, old_evt_id = None, **kwargs):
+    global CLIENT_HISTORY_MSG_EVT
+    new_evt_rt: RelatesTo = None
+    if old_evt_id:
+        new_evt_rt = RelatesTo( rel_type=RelationType.REPLACE, event_id=old_evt_id )
+    new_evt_id = await app.CLIENT.send_notice( room_id, relates_to=new_evt_rt, **kwargs )
+    await asyncio.sleep( 2 )
+    i = 0
+    async for evt in aiter_messages( app.CLIENT, room_id, PaginationDirection.BACKWARD ):
+        i += 1
+        if evt.event_id == new_evt_id:
+            if not old_evt_id:
+                CLIENT_HISTORY_MSG_EVT = evt
+            break
+        if i > 10:
+            raise RuntimeError( "Message failed to send" )
+
+
+@app.on( app.CustomEventType.POST_SHUTDOWN, False )
+async def post_shutdown (_client: Client, _evt_type: app.CustomEventType):
+    write_history( CLIENT_HISTORY_MSG_EVT )
--- info_player_start/main.py
+++ info_player_start/main.py
@@ -1,8 +1,24 @@
 import platform
+import os
 import asyncio
+from secrets import token_hex
+from mautrix.client import Client
 from dotenv import load_dotenv as __load_dotenv
 __load_dotenv()
-from . import app
+from . import app, history
+
+
+@app.on( app.CustomEventType.POST_SYNC )
+async def update (client: Client, _evt_type: app.CustomEventType):
+    try:
+        room_id = os.environ.get( "BM_IPS_ROOM_STATS_ID" )
+        await history.edit_or_write_history(
+            room_id
+            , await history.get_history_id( room_id )
+            , text=token_hex( 16 )
+        )
+    finally:
+        client.stop()
 
 
 if __name__ == "__main__":
